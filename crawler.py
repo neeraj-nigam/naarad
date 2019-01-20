ff0d97aa7387c811ecddcc6b0547df1f24548038
Neeraj Nigam <neeraj@localhost.localdomain>
2019-01-20 22:13:02 +0530

    modified crawler with class based code. modified readme file for clarity

--- crawler.py
+++ crawler.py
@@ -1,8 +1,23 @@
 import subprocess
 
+class Crawler():
+    ports, filename, base_ip, subnet, target
+
+    def __init__(self, target, base_ip, ports=[80,443], subnet=24):
+        self.ports = ports
+        self.base_ip = base_ip
+        self.target = target
+        self.subnet = subnet
+
+    def crawl(self):
+        # write code for crawler to crawl and generate output xml somewhere on the disk
+
+
+
+"""
 ports = '80,443'
 filename = 'rlt.xml'
 base_ip = '27.7.6.123'
 subnet = str(24)
 call = subprocess.Popen(['masscan','-p'+ports,base_ip+'/'+subnet,'--banners','-oX',filename])
-
+"""
