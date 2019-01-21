2de02123b7dacf50c72714f7738ccc90fe749f44
Neeraj Nigam <neeraj@localhost.localdomain>
2019-01-21 06:37:44 -0500

    arranged code

--- bin/database/db.py
+++ bin/database/db.py
@@ -4,6 +4,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
 
-engine = create_engine()
+engine = create_engine("sqlite:///:memory:",echo=True)
 Base = declarative_base()
 
+from .models import Ip, Banners, Server
+
+def create_all(self):
+    self.Base.metadata.create_all()
--- bin/database/models.py
+++ bin/database/models.py
@@ -1,4 +1,3 @@
-# from sqlalchemy import create_engine
 from .db import Base
 from sqlalchemy import Column, Integer, String
 
